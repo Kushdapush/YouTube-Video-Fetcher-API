7fd800925807610112f6e75fd79dc9f50869e974
Kushagra Gupta <kushagragupta625@gmail.com>
2025-05-23 19:33:44 +0530

    Implement API views for fetching and searching videos

--- videos/views.py
+++ videos/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
+from .models import Video
+from .serializers import VideoSerializer
+
+class VideoPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+class VideoViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+    pagination_class = VideoPagination
